9e6c709c6f5d6092557337a576c04e0e3a913cd9
Jackson Miras <jackmiras@gmail.com>
2023-07-23 19:06:48 -0300

    14 - Blade: The Absolute Basics

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,15 +4,17 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-    <?php foreach($posts as $post) : ?>
+    @foreach ($posts as $post)
         <article>
             <h1>
-                <a href="/post/<?= $post->slug ?>">
-                    <?= $post->title; ?>
+                <a href="/post/{{ $post->slug }}">
+                    {{ $post->title }}
                 </a>
             </h1>
 
-            <div><?= $post->excerpt ?></div>
+            <div>
+                {{ $post->excerpt }}
+            </div>
         </article>
-    <?php endforeach ?>
+    @endforeach
 </body>
