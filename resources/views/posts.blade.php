f754b628910e828e444aa0ccf96fc50556e1a3e4
Jackson Miras <jackmiras@gmail.com>
2023-07-27 11:17:17 -0300

    31 - Convert the HTML and CSS to Blade

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,19 +1,18 @@
 <x-layout>
-    @foreach ($posts as $post)
-        <article>
-            <h1>
-                <a href="/post/{{ $post->slug }}">
-                    {{ $post->title }}
-                </a>
-            </h1>
+    @include("partial-posts-header")
 
-            <p>
-                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="#">{{ $post->category->name }}</a>
-            </p>
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card/>
 
-            <div>
-                {{ $post->excerpt }}
-            </div>
-        </article>
-    @endforeach
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card/>
+            <x-post-card/>
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card/>
+            <x-post-card/>
+            <x-post-card/>
+        </div>
+    </main>
 </x-layout>
