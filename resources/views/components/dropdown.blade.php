d11303706a8737b7d865b629003e8ffd55cd782b
Jackson Miras <jackmiras@gmail.com>
2023-08-01 18:45:44 -0300

    35 - How to Extract a Dropdown Blade Component

---
+++ resources/views/components/dropdown.blade.php
@@ -0,0 +1,13 @@
+@props(['trigger'])
+
+<div x-data="{ show: false }" @click.away="show = false">
+    {{-- Trigger | The x-data being set is from Alpine.js --}}
+    <div @click="show = !show">
+        {{ $trigger }}
+    </div>
+
+    {{-- Links | The x-show being set is from Alpine.js --}}
+    <div x-show="show" class="py-2 absolute bg-gray-100 mt-2 rounded-xl w-full z-50" style="display: none">
+        {{ $slot }}
+    </div>
+</div>
