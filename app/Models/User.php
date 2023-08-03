61b1cde4eb51c2a7368e88c11b8724b5e08aa7cd
Jackson Miras <jackmiras@gmail.com>
2023-08-03 16:26:18 -0300

    46 - Automatic Password Hashing With Mutators

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,11 +13,6 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'username',
@@ -25,25 +20,20 @@ class User extends Authenticatable
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function setPasswordAttribute(string $password): void
+    {
+        $this->attributes["password"] = bcrypt($password);
+    }
 
     public function posts(): HasMany
     {
