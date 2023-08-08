282927a3ae996d2ff5f17bdbf1b858c01b0ba7e0
Jackson Miras <jackmiras@gmail.com>
2023-08-07 21:09:47 -0300

    60 - Extract a Newsletter Service

---
+++ app/Http/Controllers/NewsletterController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Services\Newsletter;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
+
+class NewsletterController extends Controller
+{
+    public function store(Request $request): Response
+    {
+        $request->validate(["email" => "required|email"]);
+
+        try {
+            (new Newsletter())->subscribe(request("email"));
+        } catch (Exception $exception) {
+            throw ValidationException::withMessages([
+                "email" => "This email could not be added to our newsletter list."
+            ]);
+        }
+
+        return redirect("/")->with("You are now signed up for our newsletter");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Services\Newsletter;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\ValidationException;
-use MailchimpMarketing\ApiClient;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,34 +15,12 @@ use MailchimpMarketing\ApiClient;
 |
 */
 
-Route::post("newsletter", function() {
-    request()->validate(["email" => "required|email"]);
-
-    $mailchimp = new ApiClient();
-
-    $mailchimp->setConfig([
-        "apiKey" => config("services.mailchimp.key"),
-        "server" => "us21",
-    ]);
-
-    try {
-        $mailchimp->lists->addListMember("520e1ec5e1", [
-            "email_address" => request("email"),
-            "status" => "subscribed",
-        ]);
-    } catch (Exception $exception) {
-        throw ValidationException::withMessages([
-            "email" => "This email could not be added to our newsletter list."
-        ]);
-    }
-
-    return redirect("/")->with("You are now signed up for our newsletter");
-});
-
 Route::get("/", "PostController@index")->name("home");
 
 Route::get("posts/{post:slug}", "PostController@show");
 Route::post("posts/{post:slug}/comments", "PostCommentsController@store");
+
+Route::post("newsletter", "NewsletterController@store");
 
 Route::get("register", "RegisterController@create")->middleware("guest");
 Route::post("register", "RegisterController@store")->middleware("guest");
